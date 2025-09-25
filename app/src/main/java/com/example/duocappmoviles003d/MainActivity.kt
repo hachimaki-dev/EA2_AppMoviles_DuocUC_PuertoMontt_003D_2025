2d1992b7cfa063508bbd15a35ab8530888a0c5e0
HachiMakiDev <hachimakidev@gmail.com>
2025-09-25 10:28:22 -0300

    explicando scafoldd, columns, row, buttoms y algunos atributos de modifier

--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -4,44 +4,73 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.duocappmoviles003d.ui.theme.DuocAppMoviles003DTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            DuocAppMoviles003DTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+       setContent {
+           renderView()
+       }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun renderView() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Mi App Genial") }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logoduoc),
+                contentDescription = "Logo de DUOC UC"
+            )
+
+            Spacer(
+                modifier = Modifier.height(20.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .background(colorResource(id = R.color.colorcitoNegro))
+                    .fillMaxWidth().padding(30.dp) ,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Button({}) {
+                    Text("Inicia sesion")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DuocAppMoviles003DTheme {
-        Greeting("Android")
-    }
-}
