c4b56b204c33c40ea2869d8c8cd38afc75ef90f9
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-09-25 10:32:31 -0300

    practica android studio

--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -1,47 +1,44 @@
+@file:kotlin.OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.duocappmoviles003d
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.duocappmoviles003d.ui.theme.DuocAppMoviles003DTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            DuocAppMoviles003DTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun renderView() {
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DuocAppMoviles003DTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
