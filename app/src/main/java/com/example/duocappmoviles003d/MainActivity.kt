c77773eaa940757ce4db003ef244b50fa5d0e541
Tavotsu <gustavosantanafue@gmail.com>
2025-09-28 19:50:28 -0300

    commit inicial

--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.duocappmoviles003d.ui.theme.DuocAppMoviles003DTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +30,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DuocAppMoviles003DTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            renderView()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun renderView(){
+    Scaffold(
+        topBar= {
+            TopAppBar(
+                title = {Text("Hola")}
+            )
+        }
+    ) {
+            paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ){
+            Image(
+                painter = painterResource(R.drawable.img),
+                contentDescription = "Barbell",
+                modifier = Modifier.padding(120.dp)
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DuocAppMoviles003DTheme {
-        Greeting("Android")
+            Spacer(
+                modifier = Modifier.height(500.dp)
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {  }
+        }
+
+
+
     }
+
+    Row {
+        Text("Hola mundo como estan")
+        Button({}) { "Soy un boton" }
+    }
+
 }
