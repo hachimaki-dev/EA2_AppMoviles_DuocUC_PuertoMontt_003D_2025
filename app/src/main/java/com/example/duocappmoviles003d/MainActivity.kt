a0c545a9babaa7fce90ce879dce051a903f73ff9
k3na1 <franaranguizi@gmail.com>
2025-10-01 14:01:47 -0300

    Clase MutableState

--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.duocappmoviles003d
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.duocappmoviles003d.ui.theme.DuocAppMoviles003DTheme
@@ -18,30 +29,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DuocAppMoviles003DTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            invocarTextField()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun invocarTextField(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DuocAppMoviles003DTheme {
-        Greeting("Android")
-    }
+    var textoQueCambia by remember { mutableStateOf("") }
+
+    TextField(
+        value = textoQueCambia,
+        onValueChange = {
+            valor -> textoQueCambia = valor
+        },
+        placeholder = {Text("Escribe acá")},
+        label = {Text("Hola")},
+        leadingIcon = {Icon(Icons.Default.AccountBox, contentDescription = null )}
+    )
 }
