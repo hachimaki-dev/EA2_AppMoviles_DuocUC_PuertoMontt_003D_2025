8a5ae96595427578cc15042592dcaa83590fa00c
Gustavo Santana <gustavosantanafue@gmail.com>
2025-10-02 00:32:06 -0300

    Act pokemon html

--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.duocappmoviles003d
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +14,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,48 +40,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            renderView()
+            pantallaPokemon()
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun renderView(){
-    Scaffold(
-        topBar= {
-            TopAppBar(
-                title = {Text("Hola")}
-            )
-        }
+fun pantallaPokemon(){
+
+    var nombrePokemon by remember { mutableStateOf("") }
+    var textoVisible by remember { mutableStateOf("Escribe el nombre del pokemon") }
+
+    Column (
+        modifier = Modifier.padding(16.dp).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
     ) {
-            paddingValues ->
-        Column(
-            modifier = Modifier.padding(paddingValues)
-        ){
-            Image(
-                painter = painterResource(R.drawable.dumbell),
-                contentDescription = "Barbell",
-                modifier = Modifier.padding(120.dp)
-            )
+        Text("Buscador de Pokemon")
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = nombrePokemon,
+            onValueChange = { newText ->
+                nombrePokemon = newText
+            },
+            label = { Text("Nombre del Pokémon") },
+            modifier = Modifier.fillMaxWidth()
+        )
 
-            Spacer(
-                modifier = Modifier.height(500.dp)
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {  }
-        }
-
-
-
-    }
-
-    Row {
-        Text("Hola mundo como estan")
-        Button({}) { "Soy un boton" }
     }
 
 }
