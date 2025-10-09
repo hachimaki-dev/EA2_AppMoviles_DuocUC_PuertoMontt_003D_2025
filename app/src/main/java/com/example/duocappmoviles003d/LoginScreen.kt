c3d3beb9c82fec9f46e92e9fdc65914e9dbd9d17
ChenteDUOC <vi.alarcong@duocuc.cl>
2025-10-09 10:34:58 -0300

    Creacion de rama Vicente ALarcon

--- app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
+++ app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
@@ -1,10 +1,16 @@
 package com.example.duocappmoviles003d
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,37 +22,56 @@ fun LoginScreen(
 ) {
     var username by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Pantalla de Login",
-            style = MaterialTheme.typography.headlineMedium
-        )
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Box(
+            modifier = Modifier.background(Color(83, 129, 179))
+        ){
 
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Nombre de usuario") },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
+            Image(
+                painter = painterResource(id = R.drawable.pig_wallpaper_phone),
+                contentDescription = "Imagen de fondo de la APP",
+                modifier = Modifier.fillMaxWidth().fillMaxSize().fillMaxHeight()
+            )
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Pantalla de Login",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = Color(0xFFFFFFFF),
+                    fontWeight = FontWeight.Bold
+                )
 
-        Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(32.dp))
 
-        Button(
-            onClick = {
-                if (username.text.isNotBlank()) {
-                    onNavigateToHome(username.text)
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text("Nombre de usuario") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedLabelColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        cursorColor = Color.White
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        if (username.text.isNotBlank()) {
+                            onNavigateToHome(username.text)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth().background(Color(0xFFFFFFFF))
+
+                ) {
+                    Text("Ir a Home")
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Ir a Home")
+            }
         }
-    }
 }
--- app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
+++ app/src/main/java/com/example/duocappmoviles003d/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
