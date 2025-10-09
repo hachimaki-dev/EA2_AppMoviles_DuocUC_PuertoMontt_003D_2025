4101bc5f7054a3ad3cb49d50a1ce6f62b985bf92
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-10-09 09:59:32 -0300

    Login app con fondo de zoro està lista

--- app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
+++ app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
@@ -1,13 +1,23 @@
 package com.example.duocappmoviles003d
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -17,36 +27,77 @@ fun LoginScreen(
     var username by remember { mutableStateOf(TextFieldValue("")) }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxWidth().fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Pantalla de Login",
-            style = MaterialTheme.typography.headlineMedium
-        )
+        Box(
+            modifier = Modifier.background(Color.Black)
+        ){
 
-        Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                alignment = Alignment.Center,
+                painter = painterResource(id = R.drawable.zoro),
+                contentDescription = "Imagen de fondo de mi app",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
 
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Nombre de usuario") },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Welcome",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                )
 
-        Button(
-            onClick = {
-                if (username.text.isNotBlank()) {
-                    onNavigateToHome(username.text)
+                Spacer(modifier = Modifier.height(32.dp))
+
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text("Nombre de usuario") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedLabelColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        cursorColor = Color.White,
+                        disabledLabelColor = Color.White,
+                        disabledBorderColor = Color.White,
+                        unfocusedLabelColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White
+
+                    ),
+                    shape = RoundedCornerShape(26.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        if (username.text.isNotBlank()) {
+                            onNavigateToHome(username.text)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Green
+                    )
+                ) {
+                    Text(text = "Ir a Home",
+                        color = Color.Black)
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Ir a Home")
+            }
         }
     }
+
 }
