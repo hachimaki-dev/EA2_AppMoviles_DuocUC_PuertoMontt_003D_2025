cd1df34bd95b0e370271b9b1f6c732cd00dbfd3f
Gustavo Santana <gustavosantanafue@gmail.com>
2025-10-20 14:56:13 -0300

    Login hecho | Falta logia del boton de acceso

--- app/src/main/java/com/example/duocappmoviles003d/ui/theme/Color.kt
+++ app/src/main/java/com/example/duocappmoviles003d/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val AzulOscuroCITT = Color(0xFF0151A1)
+val TurquesaCITT = Color(0xFF25EFD2)
+val FondoCITT = Color(0xFFF5F7FA)
+val TextoPistaCITT = Color(0xFFB4B4B4)
