86fa369c17b337cd94f34004a8e3c1e40ceb974e
KenzoDev1 <car.bittner@duocuc.cl>
2025-10-23 09:30:19 -0300

    subiendo archivos faltantes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.duocappmoviles003d"
+    namespace = "com.example.perfulandia"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.duocappmoviles003d"
-        minSdk = 28
+        applicationId = "com.example.perfulandia"
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -45,18 +45,18 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    //Comentarios
-    //Agregamos la dependencia del Nav
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose) // Dependencia agregadada para usar Navegation Compose
+    implementation("androidx.compose.material:material-icons-extended") // Dependencia agregada para usar mas iconos
+    implementation(libs.ads.mobile.sdk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "DuocAppMoviles003D"
+rootProject.name = "Perfulandia"
 include(":app")
