1ab62901ef6b1407e5c0783958730bf1a2768ce7
FrancisMoya1745 <franc.moyam@duocuc.cl>
2025-10-23 08:54:11 -0300

    francis

--- app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
+++ app/src/main/java/com/example/duocappmoviles003d/LoginScreen.kt
@@ -1,14 +1,18 @@
 package com.example.duocappmoviles003d
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun LoginScreen(
@@ -16,37 +20,70 @@ fun LoginScreen(
 ) {
     var username by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Pantalla de Login",
-            style = MaterialTheme.typography.headlineMedium
+
+        Image(
+            painter = painterResource(id = R.drawable.cd31minutos),
+            contentDescription = "Imagen de fondo de mi app",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
 
-        Spacer(modifier = Modifier.height(32.dp))
 
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Nombre de usuario") },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                if (username.text.isNotBlank()) {
-                    onNavigateToHome(username.text)
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Ir a Home")
+            Text(
+                text = "Pantalla de Login",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color(0xFF000000),
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Nombre de usuario") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth (),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedTextColor = Color(0xFFFFFFFF),
+                    unfocusedTextColor = Color(0xFFFFFFFF),
+                    cursorColor = Color(0xFFFFFFFF),
+                    focusedLabelColor = Color(0xFFFFFFFF),
+                    unfocusedLabelColor = Color.Gray,
+                    focusedBorderColor = Color(0xFFB71C1C),
+                    unfocusedBorderColor = Color(0xFFB71C1C)
+                )
+            )
+
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    if (username.text.isNotBlank()) {
+                        onNavigateToHome(username.text)
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFB71C1C),
+                    contentColor = Color.White
+                )
+            ) {
+                Text("Ir a Home")
+            }
+
         }
     }
 }
