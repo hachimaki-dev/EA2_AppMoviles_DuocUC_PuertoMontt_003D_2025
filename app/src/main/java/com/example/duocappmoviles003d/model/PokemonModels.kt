a98849981abae68e1992e2fd85a71eccaad71712
DiVilota <di.villota@duocuc.cl>
2025-10-29 22:33:53 -0300

    Cambios en PokemonModels.kt, Añadida función para mapear DTO y extraer la id del Pokemon

--- app/src/main/java/com/example/duocappmoviles003d/model/PokemonModels.kt
+++ app/src/main/java/com/example/duocappmoviles003d/model/PokemonModels.kt
@@ -1,5 +1,6 @@
 package com.example.duocappmoviles003d.model
 
+// DTOs que vienen de la API
 data class PokemonResponse(
     val results: List<PokemonDto>
 )
@@ -9,7 +10,20 @@ data class PokemonDto(
     val url: String
 )
 
+// Modelo que usara la UI
 data class Pokemon(
     val name: String,
     val imageUrl: String
 )
+
+// Función de extensión para mapear explícitamente DTO -> Modelo
+fun List<PokemonDto>.toPokemonList(): List<Pokemon> {
+    return this.map { dto: PokemonDto ->
+        // extraer el id desde la URL de detalle del pokemon :)
+        val id = dto.url.trimEnd('/').substringAfterLast('/')
+        Pokemon(
+            name = dto.name.replaceFirstChar { it.uppercase() },
+            imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+        )
+    }
+}
