6465696744ecc9eb90ab7817541c7b94a8c65d96
Tomas <to.garciab@duocuc.cl>
2025-11-07 23:19:50 -0300

    Haciendo la vista del carrito

---
+++ app/src/main/java/com/example/ferremas/screens/home/cart/CartItemCard.kt
@@ -0,0 +1,72 @@
+package com.example.ferremas.screens.home.cart
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.ferremas.model.Product
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+
+@Composable
+fun CartItemCard(item: Product, onRemoveItem: ()->Unit){
+    Card(modifier = Modifier.fillMaxWidth().
+    padding(vertical = 8.dp), elevation = CardDefaults.cardElevation(2.dp)){
+
+        Row(modifier = Modifier.fillMaxWidth()
+            .padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+
+            Image(
+                painter = rememberAsyncImagePainter(model = item.imageUrl),
+                contentDescription = "Product Image",
+                modifier = Modifier.size(80.dp).clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = item.name, style = MaterialTheme.typography.titleMedium)
+                Text(text = "$${item.price}", style = MaterialTheme.typography.bodyMedium)
+
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = item.name,
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        style = MaterialTheme.typography.bodyLarge)
+
+                }
+
+                IconButton(onClick= onRemoveItem, modifier = Modifier.size(24.dp)) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Remove Item")
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
--- app/src/main/java/com/example/ferremas/screens/home/cart/CartScreen.kt
+++ app/src/main/java/com/example/ferremas/screens/home/cart/CartScreen.kt
@@ -1,3 +1,70 @@
 package com.example.ferremas.screens.home.cart
 
-fun CartScreen(){}
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ferremas.R
+import com.example.ferremas.model.Product
+@Composable
+fun CartScreen(){
+    var cartItems = listOf(
+        Product("3", "Sierra", 999.9, imageUrl = R.drawable.traladro),
+    )
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp) ) {
+        Text(text = "Carrito de compras", style = MaterialTheme.
+            typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 16.dp))
+
+        if (cartItems.isEmpty()){
+            Column(modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {  }
+
+                Text(
+                    text = "El carrito está vacío",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {/**el boton de clcik**/}
+                ){
+                    Text(text = "Continuar comprando")
+                }
+        }else{
+
+            LazyColumn (modifier = Modifier.weight(14f)){
+
+                items( cartItems){item ->
+                    CardItemCard(
+                        item = item,
+                        onRemoveItem = {/**aqui se controla el remover item**/}
+                    )
+                }
+
+            }
+
+            // total y checkout
+
+        }
+    }
+
+}
+
+@Composable
+fun CardItemCard(onRemoveItem: () -> Unit, item: Product) {
+    TODO("Not yet implemented")
+}
